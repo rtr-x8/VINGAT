fb21ed706eb2896d112ab27af9eaced356dc2e9a
RyutaroM <ryu.2.24@hotmail.co.jp>
2024-11-25 19:21:41 +0900

    setup

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
+import os
+import pkg_resources
 
 setup(
-    name='vingat',  # パッケージ名
-    version='0.1',  # バージョン
-    packages=find_packages(),  # サブディレクトリに含まれるモジュールを自動で探す
-    install_requires=[  # 依存パッケージのリスト
-        'numpy',
-        'pandas',
-    ],
+  name='vingat',  # パッケージ名
+  py_modules=["vingat"],
+  version='0.1',  # バージョン
+  packages=find_packages(),  # サブディレクトリに含まれるモジュールを自動で探す
+  install_requires=[
+    str(r)
+    for r in pkg_resources.parse_requirements(
+      open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+    )
+  ],
 )
