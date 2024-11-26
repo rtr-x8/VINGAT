b048673f145146e51e09d12b0529096cfe7666f9
RyutaroM <ryu.2.24@hotmail.co.jp>
2024-11-27 00:44:51 +0900

    fix lint

--- vingat/dataloader.py
+++ vingat/dataloader.py
@@ -1,6 +1,7 @@
 from torch_geometric.data import HeteroData
 from sklearn.preprocessing import LabelEncoder
 import torch
+import torch.tensor as tensor
 from enum import Enum
 import numpy as np
 from .loader import use_nutritions
@@ -27,45 +28,60 @@ def create_hetrodata(ratings: pd.DataFrame,
     # 全てのユーザーノードを登録する必要がる
     user_features = user_label_encoder.classes_
     user_indices = np.arange(0, len(user_label_encoder.classes_))
-    hetro["user"].x = torch.tensor(user_features, dtype=torch.float32).unsqueeze(1)
-    hetro["user"].id = torch.tensor(user_indices, dtype=torch.long)  # 連番に振り直す
+    user_x = tensor(user_features, dtype=torch.float32).unsqueeze(1)
+    hetro["user"].x = user_x
+    hetro["user"].id = tensor(user_indices, dtype=torch.long)  # 連番に振り直す
     hetro["user"].num_nodes = len(user_features)
 
     recipe_features = recipe_nutrients.loc[recipe_label_encoder.classes_, use_nutritions].values
     #  recipe_indices = np.arange(0, len(recipe_label_encoder.classes_))
-    hetro["recipe"].x = torch.tensor(recipe_features, dtype=torch.float32)
-    hetro["recipe"].id = torch.tensor(recipe_label_encoder.classes_, dtype=torch.long)
+    hetro["recipe"].x = tensor(recipe_features, dtype=torch.float32)
+    hetro["recipe"].id = tensor(recipe_label_encoder.classes_, dtype=torch.long)
     hetro["recipe"].num_nodes = len(recipe_label_encoder.classes_)
-    hetro["recipe"].visual_feature = torch.ones((len(recipe_label_encoder.classes_), 1), dtype=torch.float32)
-    hetro["recipe"].intention_feature = torch.ones((len(recipe_label_encoder.classes_), 1), dtype=torch.float32)
-    hetro["recipe"].taste_feature = torch.ones((len(recipe_label_encoder.classes_), 1), dtype=torch.float32)
+    hetro["recipe"].visual_feature = torch.ones(
+        (len(recipe_label_encoder.classes_), 1), 
+        dtype=torch.float32)
+    hetro["recipe"].intention_feature = torch.ones(
+        (len(recipe_label_encoder.classes_), 1),
+        dtype=torch.float32)
+    hetro["recipe"].taste_feature = torch.ones(
+        (len(recipe_label_encoder.classes_), 1),
+        dtype=torch.float32)
 
     ingredient_features = ingredient_label_encoder.classes_
     #  ingredient_indices = np.arange(0, len(ingredient_label_encoder.classes_))
-    hetro["ingredient"].x = torch.tensor(ingredient_features, dtype=torch.float32)
-    hetro["ingredient"].id = torch.tensor(ingredient_label_encoder.classes_, dtype=torch.long)
+    hetro["ingredient"].x = tensor(ingredient_features, dtype=torch.float32)
+    hetro["ingredient"].id = tensor(ingredient_label_encoder.classes_, dtype=torch.long)
     hetro["ingredient"].num_nodes = len(ingredient_label_encoder.classes_)
 
     # edgeはデータに基づくリンクだけ設定する。
-    edge_index_user_recipe = np.array([user_label_encoder.transform(ratings["user_id"]), recipe_label_encoder.transform(ratings["recipe_id"])])
-    hetro["user", "buys", "recipe"].edge_index = torch.tensor(edge_index_user_recipe, dtype=torch.long)
-    hetro["recipe", "bought_by", "user"].edge_index = torch.tensor(edge_index_user_recipe, dtype=torch.long).flip(0)
+    edge_index_user_recipe = np.array([
+        user_label_encoder.transform(ratings["user_id"]), 
+        recipe_label_encoder.transform(ratings["recipe_id"])
+    ])
+    hetro["user", "buys", "recipe"].edge_index = tensor(edge_index_user_recipe, dtype=torch.long)
+    hetro["recipe", "bought_by", "user"].edge_index = tensor(edge_index_user_recipe, dtype=torch.long).flip(0)
 
     # 自己ループ
-    self_loop_edges = torch.arange(len(recipe_label_encoder.classes_)).unsqueeze(0).repeat(2, 1)
+    self_loop_edges = torch.arange(len(recipe_label_encoder.classes_))
+    self_loop_edges = self_loop_edges.unsqueeze(0).repeat(2, 1)
     hetro['recipe', 'self_loop', 'recipe'].edge_index = self_loop_edges
 
     # for LinkNeighborLoader
-    hetro['user', 'buys', 'recipe'].edge_label = torch.ones(edge_index_user_recipe.shape[1], dtype=torch.long)
-    hetro["user", "buys", "recipe"].edge_label_index = torch.tensor(edge_index_user_recipe, dtype=torch.long)
+    hetro['user', 'buys', 'recipe'].edge_label = torch.ones(
+        edge_index_user_recipe.shape[1], 
+        dtype=torch.long)
+    hetro["user", "buys", "recipe"].edge_label_index = tensor(edge_index_user_recipe, dtype=torch.long)
 
-    edge_index_ingredient_recipe = np.array([ingredient_label_encoder.transform(recipe_ingredients["ingredient_id"]),
-                                            recipe_label_encoder.transform(recipe_ingredients["recipe_id"])])
-    hetro["ingredient", "used_in", "recipe"].edge_index = torch.tensor(edge_index_ingredient_recipe, dtype=torch.long)
+    edge_index_ingredient_recipe = np.array([
+        ingredient_label_encoder.transform(recipe_ingredients["ingredient_id"]),
+        recipe_label_encoder.transform(recipe_ingredients["recipe_id"])])
+    hetro["ingredient", "used_in", "recipe"].edge_index = tensor(edge_index_ingredient_recipe, dtype=torch.long)
 
     hetro.to(device)
 
     return hetro
+
 
 def create_dataloader(
     core_train_rating: pd.DataFrame,
@@ -76,8 +92,18 @@ def create_dataloader(
     recipe_nutrients: pd.DataFrame,
     device: torch.device
 ):
-    all_user_id = pd.concat([core_train_rating["user_id"], core_test_rating["user_id"], core_val_rating["user_id"]]).unique()
-    all_recipe_id = pd.concat([core_train_rating["recipe_id"], core_test_rating["recipe_id"], core_val_rating["recipe_id"], recipe_ingredients["recipe_id"]]).unique()
+    all_user_id = pd.concat([
+        core_train_rating["user_id"], 
+        core_test_rating["user_id"], 
+        core_val_rating["user_id"]
+    ]).unique()
+    
+    all_recipe_id = pd.concat([
+        core_train_rating["recipe_id"], 
+        core_test_rating["recipe_id"], 
+        core_val_rating["recipe_id"], 
+        recipe_ingredients["recipe_id"]
+    ]).unique()
 
     user_label_encoder = LabelEncoder().fit(all_user_id)
     recipe_label_encoder = LabelEncoder().fit(all_recipe_id)
--- vingat/loader.py
+++ vingat/loader.py
@@ -10,9 +10,10 @@ import torch
 from tqdm.notebook import tqdm
 
 
-use_nutritions = ["niacin", "fiber","sugars", "sodium", "carbohydrates", "vitaminB6",
-                  "calories", "thiamin", "fat", "folate", "caloriesFromFat", "calcium",
-                  "magnesium", "iron", "cholesterol", "protein", "vitaminA", "potassium",
+use_nutritions = ["niacin", "fiber","sugars", "sodium", "carbohydrates", 
+                  "vitaminB6", "calories", "thiamin", "fat", "folate",
+                  "caloriesFromFat", "calcium", "magnesium", "iron",
+                  "cholesterol", "protein", "vitaminA", "potassium",
                   "saturatedFat", "vitaminC"]
 
 
@@ -22,7 +23,9 @@ def parse_nutrient_json(json_dict):
         res.update({un: eval(json_dict).get(un).get("amount")})
     return res
 
-def core_file_loader(directory_path: str) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def core_file_loader(
+        directory_path: str
+    ) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     return (
         pd.read_csv(f"{directory_path}/core-data_recipe.csv", index_col=0),
         pd.read_csv(f"{directory_path}/core-data-train_rating.csv"),
@@ -33,7 +36,8 @@ def core_file_loader(directory_path: str) -> tuple[pd.DataFrame, pd.DataFrame, p
 
 """ アイテム - 栄養素の一次保存データ """
 def load_recipe_nutrients(directory_path: str, originarl_df: pd.DataFrame):
-    if not os.path.isfile(f"{directory_path}/recipe_nutrients.csv"):
+    file_path = f"{directory_path}/recipe_nutrients.csv"
+    if not os.path.isfile(file_path):
 
         __recipes = originarl_df.copy()
         __recipes[use_nutritions] = 0.0
@@ -45,9 +49,9 @@ def load_recipe_nutrients(directory_path: str, originarl_df: pd.DataFrame):
             for key, val in nutri_val.items():
                 __recipes.loc[idx, key] = val
 
-        __recipes.to_csv(f"{directory_path}/recipe_nutrients.csv")
+        __recipes.to_csv(file_path)
 
-    recipe_nutrients = pd.read_csv(f"{directory_path}/recipe_nutrients.csv", index_col=0)
+    recipe_nutrients = pd.read_csv(file_path, index_col=0)
     print("recipe_nutrients is loaded")
     return recipe_nutrients
 
@@ -72,64 +76,95 @@ def load_ingredients(directory_path: str, originarl_df: pd.DataFrame):
 
 """ アイテム - 食材の一時保存データ """
 def load_recipe_ingredients(directory_path: str, originarl_df: pd.DataFrame):
-    if not os.path.isfile(f"{directory_path}/recipe_ingredients.csv"):
+    file_path = f"{directory_path}/recipe_ingredients.csv"
+    if not os.path.isfile(file_path):
 
         __recipes = originarl_df.copy()
-        recipe_ingredients = pd.DataFrame([], columns=["recipe_id", "ingredient_id"])
+        recipe_ingredients = pd.DataFrame([], columns=["recipe_id", 
+                                                       "ingredient_id"])
         ingredients = load_ingredients(directory_path, originarl_df)
         all_ings = ingredients.values.reshape(-1)
-        for recipe_id, recipe_row in tqdm(__recipes.iterrows(), total = __recipes.shape[0]):
+        for recipe_id, recipe_row in tqdm(__recipes.iterrows(), total=__recipes.shape[0]):
             for recip_ing in recipe_row["ingredients"].split("^"):
                 if recip_ing in all_ings:
-                    recipe_ingredients = pd.concat([recipe_ingredients,
-                                                    pd.DataFrame([[recipe_id, np.where(all_ings == recip_ing)[0][0]]],
-                                                                columns=["recipe_id", "ingredient_id"])])
-        recipe_ingredients.to_csv(f"{directory_path}/recipe_ingredients.csv")
+                    recipe_ingredients = pd.concat([
+                        recipe_ingredients,
+                        pd.DataFrame(
+                            [[
+                                recipe_id, 
+                                np.where(all_ings == recip_ing)[0][0]
+                            ]],
+                            columns=["recipe_id", "ingredient_id"])]
+                        )
+        recipe_ingredients.to_csv(file_path)
 
-    recipe_ingredients = pd.read_csv(f"{directory_path}/recipe_ingredients.csv", index_col=0)
+    recipe_ingredients = pd.read_csv(file_path, index_col=0)
     print("recipe_ingredients is loaded")
     return recipe_ingredients
 
 
 """レシピ名、調理工程、レシピ画像の一時保存データ"""
-def load_recipe_cooking_directions(directory_path: str, originarl_df: pd.DataFrame):
-    if not os.path.isfile(f"{directory_path}/recipe_cooking_directions.csv"):
+def load_recipe_cooking_directions(
+        directory_path: str, 
+        originarl_df: pd.DataFrame
+    ) -> pd.DataFrame:
+    file_path = f"{directory_path}/recipe_cooking_directions.csv"
+    key = file_path
+    if not os.path.isfile(file_path):
 
         __recipes = originarl_df.copy()
-        __recipes["cooking_directions"] = __recipes["cooking_directions"].to_dict()
+        __recipes[key] = __recipes[key].to_dict()
 
-        recipe_cooking_directions = pd.DataFrame([], index=__recipes.index, columns=["direction"])
+        recipe_cooking_directions = pd.DataFrame(
+            [], 
+            index=__recipes.index,
+            columns=["direction"]
+        )
         for i in tqdm(__recipes.index.to_list(), total = __recipes.shape[0]):
-            txt = __recipes.at[i, "cooking_directions"]
+            txt = __recipes.at[i, key]
             txt = eval(txt).get('directions')
             txts = txt.split("\n")
             txts = [t for t in txts if len(t) > 10]
             txt = "\n".join(txts)
             recipe_cooking_directions.at[i, "direction"] = txt
-        recipe_cooking_directions.to_csv(f"{directory_path}/recipe_cooking_directions.csv")
+        recipe_cooking_directions.to_csv(file_path)
 
-    recipe_cooking_directions = pd.read_csv(f"{directory_path}/recipe_cooking_directions.csv", index_col=0)
+    recipe_cooking_directions = pd.read_csv(file_path, index_col=0)
     print("recipe_cooking_directions is loaded")
     return recipe_cooking_directions
 
 
 """ 食材の埋込データ """
-def load_ingredients_with_embeddings(directory_path: str, originarl_df: pd.DataFrame):
-    if not os.path.isfile(f"{directory_path}/ingredients_with_embeddings.csv"):
-        raise Exception("embedding file not found") # next cell, next next cell, next next next cell
-    ingredients_with_embeddings = pd.read_csv(f"{directory_path}/ingredients_with_embeddings.csv", index_col=0)
+def load_ingredients_with_embeddings(
+        directory_path: str,
+        originarl_df: pd.DataFrame
+    ):
+    file_path = f"{directory_path}/ingredients_with_embeddings.csv"
+    if not os.path.isfile(file_path):
+         # next cell, next next cell, next next next cell
+        raise Exception("embedding file not found")
+    ingredients_with_embeddings = pd.read_csv(file_path, index_col=0)
     print("ingredients_with_embeddings is loaded")
     return ingredients_with_embeddings
 
 
 """ レシピ画像の特徴データ """
-def load_recipe_image_embeddings(directory_path: str, originarl_df: pd.DataFrame, device):
-    if not os.path.isfile(f"{directory_path}/recipe_image_embeddings.csv"):
+def load_recipe_image_embeddings(
+        directory_path: str, 
+        originarl_df: pd.DataFrame, device
+    ) -> pd.DataFrame:
+    file_path = f"{directory_path}/recipe_image_embeddings.csv"
+    if not os.path.isfile(file_path):
         cnn_model = models.resnet50(pretrained=True)
         cnn = nn.Sequential(
-            *(list(cnn_model.children())[:-2]),  # 最後の平均プーリング層の手前まで取得
-            nn.Conv2d(2048, 1024, kernel_size=1),  # 1x1畳み込み層でoutput_dim次元に
-            nn.AdaptiveAvgPool2d((1, 1))  # 出力を1x1にプールしてoutput_dim次元ベクトルに変換
+            # 最後の平均プーリング層の手前まで取得
+            *(list(cnn_model.children())[:-2]),
+            
+            # 1x1畳み込み層でoutput_dim次元に
+            nn.Conv2d(2048, 1024, kernel_size=1),
+            
+            # 出力を1x1にプールしてoutput_dim次元ベクトルに変換
+            nn.AdaptiveAvgPool2d((1, 1))
         ).to(device)
 
         # 評価モードに設定
@@ -150,7 +185,10 @@ def load_recipe_image_embeddings(directory_path: str, originarl_df: pd.DataFrame
         ])
 
         recipe_ingredients = load_recipe_ingredients(directory_path, originarl_df)
-        recipe_image_embeddings = pd.DataFrame([], index=recipe_ingredients["recipe_id"].unique(), columns=[f"e_{i}" for i in range(1024)])
+        recipe_image_embeddings = pd.DataFrame(
+            [], 
+            index=recipe_ingredients["recipe_id"].unique(), 
+            columns=[f"e_{i}" for i in range(1024)])
         errors = []
 
         def process_image(recipe_id):
@@ -158,7 +196,7 @@ def load_recipe_image_embeddings(directory_path: str, originarl_df: pd.DataFrame
             try:
                 image_path = f"{directory_path}/core-data-images/core-data-images/{recipe_id}.jpg"
                 image = io.read_image(image_path).to(device)
-                image = image_transform(image).unsqueeze(0).to(device)  # 前処理とデバイス移動
+                image = image_transform(image).unsqueeze(0).to(device)
 
                 with torch.no_grad():
                     output = cnn(image)
@@ -172,16 +210,17 @@ def load_recipe_image_embeddings(directory_path: str, originarl_df: pd.DataFrame
             return recipe_id, output.view(-1).cpu().numpy()
 
         # 画像をバッチ処理
-        with ThreadPoolExecutor(max_workers=4) as executor:  # 並列処理のスレッド数を調整
-            results = list(tqdm(executor.map(process_image, recipe_image_embeddings.index.tolist()),
-                                total=len(recipe_image_embeddings.index.tolist()),
-                                desc="Image Embedding gen"))
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            results = list(tqdm(
+                executor.map(process_image, recipe_image_embeddings.index.tolist()),
+                total=len(recipe_image_embeddings.index.tolist()),
+                desc="Image Embedding gen"))
         for recipe_id, embedding in tqdm(results, desc="save..."):
             recipe_image_embeddings.loc[recipe_id] = embedding
 
         # 結果をCSVに保存
-        recipe_image_embeddings.to_csv(f"{directory_path}/recipe_image_embeddings.csv")
+        recipe_image_embeddings.to_csv(file_path)
 
-    recipe_image_embeddings = pd.read_csv(f"{directory_path}/recipe_image_embeddings.csv", index_col=0)
+    recipe_image_embeddings = pd.read_csv(file_path, index_col=0)
     print("recipe_image_embeddings is loaded")
     return recipe_image_embeddings
