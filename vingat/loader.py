5d4ad59247aa35fb235060d2a31efc871b4ff478
RyutaroM <ryu.2.24@hotmail.co.jp>
2024-11-26 13:44:35 +0900

    fix

--- vingat/loader.py
+++ vingat/loader.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def core_file_loader(directory_path: str) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame):
     return (
-        pd.read_csv(f"{directory_path}/core-data_recipe.csv", index_col=0)
-        pd.read_csv(f"{directory_path}/core-data-train_rating.csv")
-        pd.read_csv(f"{directory_path}/core-data-test_rating.csv")
+        pd.read_csv(f"{directory_path}/core-data_recipe.csv", index_col=0),
+        pd.read_csv(f"{directory_path}/core-data-train_rating.csv"),
+        pd.read_csv(f"{directory_path}/core-data-test_rating.csv"),
         pd.read_csv(f"{directory_path}/core-data-valid_rating.csv")
     )
