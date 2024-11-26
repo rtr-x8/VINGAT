53512c4ec2badf6dc4b8f3be16e207750e00bdbe
RyutaroM <ryu.2.24@hotmail.co.jp>
2024-11-26 14:23:56 +0900

    add logger

--- vingat/logger.py
+++ vingat/logger.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 import pytz
 
-def get_run_name():
+def get_current_time() -> str:
   japan_tz = pytz.timezone('Asia/Tokyo')
-  current_time_jst = datetime.now(japan_tz).strftime("%Y%m%d-%H%M%S")
-  run_name = f"run-{current_time_jst}"
+  return datetime.now(japan_tz).strftime("%Y%m%d-%H%M%S")
+
+def get_run_name():
+  run_name = f"run-{get_current_time()}"
 
