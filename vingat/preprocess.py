36ca6acbad7a2f6e38886ea0eec6ed21317fc3b4
RyutaroM <ryu.2.24@hotmail.co.jp>
2024-12-19 02:38:39 +0900

    add preprocess

--- vingat/preprocess.py
+++ vingat/preprocess.py
@@ -1,2 +1,89 @@
-def testfunc(in1, in2):
-    return in1 + in2
+import torch
+
+
+class MinMaxScaler:
+    def __init__(self):
+        self.min = None
+        self.max = None
+
+    def fit(self, x):
+        self.min = torch.min(x, dim=0).values
+        self.max = torch.max(x, dim=0).values
+        self.denom = self.max - self.min
+        self.denom[self.denom == 0] = 1  # Prevent division by zero
+
+    def transform(self, x):
+        return (x - self.min) / self.denom
+
+
+class BasePreprocess():
+    def __init__(self):
+        pass
+
+    def fit():
+        raise NotImplementedError
+
+    def transform():
+        raise NotImplementedError
+
+
+class UserPreprocess(BasePreprocess):
+    def __init__(self):
+        self.scaler = MinMaxScaler()
+
+    def fit(self, x: torch.Tensor):
+        self.scaler.fit(self.data)
+
+    def transform(self, x: torch.Tensor):
+        return self.scaler.transform(x)
+
+
+def userPreprocess(train, target):
+    userPreprocess = UserPreprocess()
+    userPreprocess.fit(train)
+    return userPreprocess, userPreprocess.transform(target)
+
+
+class RecipePreprocess(BasePreprocess):
+    def __init__(self):
+        self.scaler = MinMaxScaler()
+
+    def fit(self, x: torch.Tensor):
+        self.scaler.fit(self.data)
+
+    def transform(self, x: torch.Tensor):
+        return self.scaler.transform(x)
+
+
+class IngredientPreprocess(BasePreprocess):
+    def __init__(self):
+        self.scaler = MinMaxScaler()
+
+    def fit(self, x: torch.Tensor):
+        self.scaler.fit(self.data)
+
+    def transform(self, x: torch.Tensor):
+        return self.scaler.transform(x)
+
+
+class IntentionPreprocess(BasePreprocess):
+    def __init__(self):
+        self.scaler = MinMaxScaler()
+
+    def fit(self, x: torch.Tensor):
+        self.scaler.fit(self.data)
+
+    def transform(self, x: torch.Tensor):
+        return self.scaler.transform(x)
+
+
+class ImagePreprocess(BasePreprocess):
+    def __init__(self):
+        self.scaler = MinMaxScaler()
+
+    def fit(self, x: torch.Tensor):
+        self.scaler.fit(self.data)
+
+    def transform(self, x: torch.Tensor):
+        return self.scaler.transform(x)
+
