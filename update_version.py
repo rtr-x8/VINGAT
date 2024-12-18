4501dcf45ae45d83e8024348967e36869957e799
RyutaroM <ryu.2.24@hotmail.co.jp>
2024-12-19 04:06:24 +0900

    fix lint

--- update_version.py
+++ update_version.py
@@ -8,7 +8,7 @@ with open(setup_file, "r") as file:
     content = file.read()
 
 # 現在のバージョンを取得
-version_pattern = r"__version__=['\"]([0-9]+\.[0-9]+\.[0-9]+)['\"]"
+version_pattern = r"__version__ = ['\"]([0-9]+\.[0-9]+\.[0-9]+)['\"]"
 current_version_match = re.search(version_pattern, content)
 if not current_version_match:
     raise ValueError("Current version not found in setup.py")
@@ -20,7 +20,7 @@ major, minor, patch = map(int, current_version.split('.'))
 new_version = f"{major}.{minor}.{patch + 1}"
 
 # setup.pyのバージョンを更新
-new_content = re.sub(version_pattern, f"__version__='{new_version}'", content)
+new_content = re.sub(version_pattern, f"__version__ = '{new_version}'", content)
 
 # setup.pyを書き戻す
 with open(setup_file, "w") as file:
