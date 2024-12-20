ce6576c6eeafe36f45c594808fd528cab4aaaf4b
RyutaroM <ryu.2.24@hotmail.co.jp>
2024-12-20 23:16:11 +0900

    remove old method and firx

--- vingat/dataloader.py
+++ vingat/dataloader.py
@@ -7,140 +7,8 @@ import pandas as pd
 from torch_geometric.loader import LinkNeighborLoader
 from vingat.encoder import StaticEmbeddingLoader
 from typing import Tuple, Optional
-from torch.nn import init
 import copy
-import math
-from vingat.loader import load_user_embeddings
 from vingat.preprocess import ScalarPreprocess
-
-
-def create_hetrodata(
-    ratings: pd.DataFrame,
-    recipe_ingredients: pd.DataFrame,
-    recipe_nutrients: pd.DataFrame,
-    user_label_encoder: LabelEncoder,
-    recipe_label_encoder: LabelEncoder,
-    ingredient_label_encoder: LabelEncoder,
-    device,
-    hidden_dim: int = 128
-) -> HeteroData:
-
-    hetro = HeteroData()
-
-    # Node #
-
-    # User nodes
-    num_users = len(user_label_encoder.classes_)
-    user_x = torch.zeros((num_users, hidden_dim), dtype=torch.float32)
-    user_id = torch.tensor(user_label_encoder.classes_, dtype=torch.long)
-    hetro["user"].x = user_x
-    hetro["user"].user_id = user_id
-    hetro["user"].id = torch.range(0, num_users - 1, dtype=torch.long)
-    hetro["user"].num_nodes = num_users
-
-    # Recipe nodes
-    num_recipes = len(recipe_label_encoder.classes_)
-    recipe_x = torch.zeros((num_recipes, hidden_dim), dtype=torch.float32)
-    recipe_id = torch.tensor(recipe_label_encoder.classes_, dtype=torch.long)
-    hetro["item"].x = recipe_x
-    hetro["item"].id = torch.range(0, num_recipes - 1, dtype=torch.long)
-    hetro["item"].recipe_id = recipe_id
-    hetro["item"].num_nodes = num_recipes
-
-    # Image nodes (one-to-one with recipes)
-    hetro["image"].x = torch.zeros((num_recipes, hidden_dim), dtype=torch.float32)
-    hetro["image"].recipe_id = recipe_id
-    hetro["image"].num_nodes = num_recipes
-
-    # Intention nodes (one-to-one with recipes)
-    nutrient_features = recipe_nutrients.loc[
-        recipe_label_encoder.classes_, use_nutritions].values
-    hetro["intention"].x = torch.zeros((num_recipes, hidden_dim), dtype=torch.float32)
-    hetro["intention"].nutrient = torch.tensor(nutrient_features, dtype=torch.float32)
-    hetro["intention"].recipe_id = recipe_id
-    hetro["intention"].num_nodes = num_recipes
-
-    # Taste nodes (one-to-one with recipes)
-    # hetro["taste"].x = cooking_direction_embeddings.to(torch.float32)
-    hetro["taste"].x = torch.zeros((num_recipes, hidden_dim), dtype=torch.float32)
-    hetro["taste"].recipe_id = recipe_id
-    hetro["taste"].num_nodes = num_recipes
-
-    # Ingredient nodes
-    num_ingredients = len(ingredient_label_encoder.classes_)
-    ingredient_x = torch.zeros((num_ingredients, hidden_dim), dtype=torch.float32)
-    ingredient_id = torch.tensor(ingredient_label_encoder.classes_, dtype=torch.long)
-    hetro["ingredient"].x = ingredient_x
-    hetro["ingredient"].ingredient_id = ingredient_id
-    hetro["ingredient"].num_nodes = num_ingredients
-
-    # Edge #
-
-    # Edges between user and recipe (buys)
-    edge_index_user_recipe = torch.tensor([
-        user_label_encoder.transform(ratings["user_id"].values),
-        recipe_label_encoder.transform(ratings["recipe_id"].values)
-    ], dtype=torch.long)
-    hetro["user", "buys", "item"].edge_index = edge_index_user_recipe
-    hetro["item", "bought_by", "user"].edge_index = edge_index_user_recipe.flip(0)
-
-    # Edges between image and recipe (one-to-one)
-    edge_index_image_recipe = torch.stack([
-        torch.arange(num_recipes),
-        torch.arange(num_recipes)
-    ], dim=0)
-    hetro["image", "associated_with", "item"].edge_index = edge_index_image_recipe
-    hetro["item", "has_image", "image"].edge_index = edge_index_image_recipe.flip(0)
-
-    # Edges between intention and recipe (one-to-one)
-    edge_index_intention_recipe = torch.stack([
-        torch.arange(num_recipes),
-        torch.arange(num_recipes)
-    ], dim=0)
-    hetro["intention", "associated_with", "item"].edge_index = edge_index_intention_recipe
-    hetro["item", "has_intention", "intention"].edge_index = edge_index_intention_recipe.flip(0)
-
-    # Edges between taste and recipe (one-to-one)
-    edge_index_taste_recipe = torch.stack([
-        torch.arange(num_recipes),
-        torch.arange(num_recipes)
-    ], dim=0)
-    hetro["taste", "associated_with", "item"].edge_index = edge_index_taste_recipe
-    hetro["item", "has_taste", "taste"].edge_index = edge_index_taste_recipe.flip(0)
-
-    # ing to taste(recupe)
-    edge_index_ingredient_recipe = np.array([
-        ingredient_label_encoder.transform(recipe_ingredients["ingredient_id"]),
-        recipe_label_encoder.transform(recipe_ingredients["recipe_id"])])
-    edge_index_taste_ingredient = torch.tensor(edge_index_ingredient_recipe, dtype=torch.long)
-    hetro["taste", "contains", "ingredient"].edge_index = edge_index_taste_ingredient
-    hetro["ingredient", "part_of", "taste"].edge_index = edge_index_taste_ingredient.flip(0)
-
-    # for LinkNeighborLoader #
-    hetro['user', 'buys', 'item'].edge_label = torch.ones(
-        edge_index_user_recipe.shape[1],
-        dtype=torch.long)
-    hetro["user", "buys", "item"].edge_label_index = torch.tensor(
-        edge_index_user_recipe, dtype=torch.long)
-
-    # edge_index の包括的なチェック
-    # ユーザーインデックスのチェック
-
-    # ノード数を取得
-    num_users = len(user_label_encoder.classes_)
-    num_recipes = len(recipe_label_encoder.classes_)
-    num_ingredients = len(ingredient_label_encoder.classes_)
-
-    # イントネーション（intention）インデックスのチェック
-
-    # 材料（ingredient）インデックスのチェック
-    if ingredient_id.max() >= num_ingredients:
-        raise ValueError("Ingredientインデックスがノード数を超えています。")
-    if ingredient_id.min() < 0:
-        raise ValueError("Ingredientインデックスに負の値が含まれています。")
-
-    hetro.to(device)
-    return hetro
 
 
 def create_dataloader(
@@ -213,17 +81,17 @@ def create_base_hetero(
     data = HeteroData()
 
     # Node
-    data["user"].num_nodes = len(user_lencoder.classes_)
+    num_users = len(user_lencoder.classes_)
+    data["user"].num_nodes = num_users
     data["user"].user_id = torch.tensor(user_lencoder.classes_)
-    user_encoder = StaticEmbeddingLoader(
-        load_user_embeddings(directory_path, user_lencoder.classes_),
-        dimention=hidden_dim, device=device)
-    data["user"].x = user_encoder(torch.tensor(user_lencoder.classes_, dtype=torch.long))
+    data["user"].x = torch.zeros((num_users, hidden_dim), dtype=torch.float32)
+    data["user"].id = torch.range(0, num_users - 1, dtype=torch.long)
 
-    data["item"].num_nodes = len(item_lencoder.classes_)
+    num_items = len(item_lencoder.classes_)
+    data["item"].num_nodes = num_items
     data["item"].item_id = torch.tensor(item_lencoder.classes_)
-    data['item'].x = torch.empty((len(item_lencoder.classes_), hidden_dim)).to(device)
-    init.kaiming_uniform_(data['item'].x, a=math.sqrt(5))
+    data['item'].x = torch.zeros((num_items, hidden_dim), dtype=torch.float32)
+    data["item"].id = torch.range(0, num_items - 1, dtype=torch.long)
 
     data["image"].num_nodes = len(item_lencoder.classes_)
     data["image"].item_id = torch.tensor(item_lencoder.classes_)
