2e76d651a897758ac2b464e7742718802cafcd0e
RyutaroM <ryu.2.24@hotmail.co.jp>
2024-12-26 02:16:58 +0900

    add metrics

--- vingat/metrics.py
+++ vingat/metrics.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch
 from typing import List
+from torchmetrics.retrieval import RetrievalRecall, RetrievalPrecision, RetrievalAUROC, RetrievalNormalizedDCG
+from torchmetrics.classification import BinaryAccuracy, BinaryPrecision, BinaryRecall, BinaryF1Score, ConfusionMatrix
 
 
 def ndcg_at_k(r: np.ndarray, k: int):
@@ -48,3 +50,58 @@ def score_stastics(pos_scores: List[torch.Tensor], neg_scores: List[torch.Tensor
             "score_metrics/diff_mean": diff_mean,
         },
     }
+
+
+class MetricsAtK():
+    def __init__(self, k: int):
+        self.k = k
+        self.recall = RetrievalRecall(top_k=k)
+        self.precision = RetrievalPrecision(top_k=k, adaptive_k=True)
+        self.auroc = RetrievalAUROC(top_k=k)
+        self.ndcg = RetrievalNormalizedDCG(top_k=k)
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor, indexed: torch.Tensor):
+        self.recall.update(preds, target, indexed)
+        self.precision.update(preds, target, indexed)
+        self.auroc.update(preds, target, indexed)
+        self.ndcg.update(preds, target, indexed)
+        return self
+
+    def compute(self, prefix):
+        return {
+            f"{prefix}recall": self.recall.compute(),
+            f"{prefix}precision": self.precision.compute(),
+            f"{prefix}auroc": self.auroc.compute(),
+            f"{prefix}ndcg": self.ndcg.compute()
+        }
+
+
+class Metrics():
+    def __init__(self, k: int):
+        self.k = k
+        self.accuracy = BinaryAccuracy(threshold=0.5)
+        self.precision = BinaryPrecision(threshold=0.5)
+        self.recall = BinaryRecall(threshold=0.5)
+        self.f1 = BinaryF1Score(threshold=0.5)
+        self.confusion_matrix = ConfusionMatrix(num_classes=2)
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor):
+        self.accuracy.update(preds, target)
+        self.precision.update(preds, target)
+        self.recall.update(preds, target)
+        self.f1.update(preds, target)
+        self.confusion_matrix.update(preds, target)
+        return self
+
+    def compute(self, prefix: str):
+        cm = self.confusion_matrix.compute()
+        return {
+            f"{prefix}accuracy": self.accuracy.compute(),
+            f"{prefix}precision": self.precision.compute(),
+            f"{prefix}recall": self.recall.compute(),
+            f"{prefix}f1": self.f1.compute(),
+            f"{prefix}true_negative": cm[0][0],
+            f"{prefix}false_positive": cm[0][1],
+            f"{prefix}false_negative": cm[1][0],
+            f"{prefix}true_positive": cm[1][1]
+        }
